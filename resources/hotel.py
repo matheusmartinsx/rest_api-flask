1d03fa00597786abb402cf3dd34d56bff482b94a
matheusmartinsx <matheusmartinsx@gmail.com>
2022-05-26 10:44:33 -0300

    Implementação do método PUT

--- resources/hotel.py
+++ resources/hotel.py
@@ -30,33 +30,41 @@ class Hoteis(Resource):
         return {'hoteis': hoteis}
 
 class Hotel(Resource):
-    def get(self, hotel_id):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('estado')
+        
+    def find_hotel(self, hotel_id):
         for hotel in hoteis:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
+        return None
+
+    def get(self, hotel_id):
+        hotel = self.find_hotel(hotel_id)
+        if hotel:
+            return hotel, 200
         return {'message':  'Hotel not found'}, 404
     
     def post(self, hotel_id):
-        argumentos = reqparse.RequestParser()
-        argumentos.add_argument('nome')
-        argumentos.add_argument('estrelas')
-        argumentos.add_argument('diaria')
-        argumentos.add_argument('estado')
+        dados = self.argumentos.parse_args()
         
-        dados = argumentos.parse_args()
+        novo_hotel = {'hotel_id': hotel_id, **dados}
         
-        novo_hotel = {
-            'hotel_id': hotel_id,
-            'nome': dados['nome'],
-            'estrelas': dados['estrelas'],
-            'diaria': dados['diaria'],
-            'estado': dados['estado']
-        }
         hoteis.append(novo_hotel)
         return novo_hotel, 200
     
     def put(self, hotel_id):
-        pass
+        dados = self.argumentos.parse_args()
+        novo_hotel = {'hotel_id': hotel_id, **dados}
+        hotel = self.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201
     
     def delete(self, hotel_id):
         pass
