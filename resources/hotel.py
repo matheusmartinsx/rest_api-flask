e7d3ab4e446cc6a2b89b90e8d1ee004433c47ace
matheusmartinsx <matheusmartinsx@gmail.com>
2022-05-26 09:55:29 -0300

    Implementação do GET Hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -10,7 +10,7 @@ hoteis = [
         'estado': 'Rio de Janeiro'
     },
     {
-        'hotel_id': 'beta',
+        'hotel_id': 'bravo',
         'nome': 'Beta Hotel',
         'estrelas': 4.0,
         'diaria': 400.10,
@@ -28,4 +28,19 @@ hoteis = [
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
+
+class Hotel(Resource):
+    def get(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return {'message':  'Hotel not found'}, 404
     
+    def post(self, hotel_id):
+        pass
+    
+    def put(self, hotel_id):
+        pass
+    
+    def delete(self, hotel_id):
+        pass
